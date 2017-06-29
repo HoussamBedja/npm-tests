375592394e65a24fee984bd9d30674e336357c91
Houssam Bedja <houssam.b@hotmail.com>
2017-06-29 01:48:51 +0000

    juggle async passed

--- program.js
+++ program.js
@@ -1,10 +1,34 @@
-// var http = require("http");
+var http = require("http");
+var results = [];
+var count = 0;
 
-// http.get(process.argv[2], function callback(response) {
-//     response.setEncoding("utf8");
-//     response.on("data", function(data) {
-//         console.log(data);
-//     });
-// })
+function httpGet(i) {
+    var info = [];
+    http.get(process.argv[2+i],function(res){
+        res.setEncoding('utf8');
+        res.on("data",function(input){
+            info.push(input);
+        });
+        res.on("error",console.error);
+        res.on("end",function(){
+            results[i] = info.join("");
+            count++;
+        if (count == 3) {
+            printResults();
+        }
+        });
+        
+        
+    });
+}
 
-var bl = require('bl');
+function printResults() {
+    for (var j=0; j<3; j++) {
+        console.log(results[j]);   
+    }
+}
+
+
+ for (var i=0; i<3; i++) {
+     httpGet(i);
+ }
