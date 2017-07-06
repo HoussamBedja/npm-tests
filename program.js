1eddc26ec6e4307995646994b8fd889b59e20f08
Houssam Bedja <houssam.b@hotmail.com>
2017-07-06 19:43:06 +0000

    net module server response

--- program.js
+++ program.js
@@ -1,34 +1,52 @@
-var http = require("http");
-var results = [];
-var count = 0;
+// var http = require("http");
+// var results = [];
+// var count = 0;
 
-function httpGet(i) {
-    var info = [];
-    http.get(process.argv[2+i],function(res){
-        res.setEncoding('utf8');
-        res.on("data",function(input){
-            info.push(input);
-        });
-        res.on("error",console.error);
-        res.on("end",function(){
-            results[i] = info.join("");
-            count++;
-        if (count == 3) {
-            printResults();
-        }
-        });
+// function httpGet(i) {
+//     var info = [];
+//     http.get(process.argv[2+i],function(res){
+//         res.setEncoding('utf8');
+//         res.on("data",function(input){
+//             info.push(input);
+//         });
+//         res.on("error",console.error);
+//         res.on("end",function(){
+//             results[i] = info.join("");
+//             count++;
+//         if (count == 3) {
+//             printResults();
+//         }
+//         });
         
         
-    });
-}
+//     });
+// }
 
-function printResults() {
-    for (var j=0; j<3; j++) {
-        console.log(results[j]);   
-    }
-}
+// function printResults() {
+//     for (var j=0; j<3; j++) {
+//         console.log(results[j]);   
+//     }
+// }
 
 
- for (var i=0; i<3; i++) {
-     httpGet(i);
- }
+//  for (var i=0; i<3; i++) {
+//      httpGet(i);
+//  }
+
+
+var net = require('net');
+var server = net.createServer(function (socket) {  
+// socket handling logic
+var date = new Date();
+var year = date.getFullYear();
+var month = date.getMonth()+1;
+var day = date.getDate();
+var hour = date.getHours();
+var minutes = date.getMinutes();
+if(month<10) month = "0"+ month;
+if(day<10) day = "0"+ day;
+var date = year+"-"+month+"-"+day+""+" "+hour+":"+minutes+"\n";
+socket.end(date);
+
+})  
+server.listen(process.argv[2]);
